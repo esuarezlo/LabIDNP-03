708aefb5477fc17f54d673621f2900d03277adef
Ernesto Suarez <ernesto@amicitech.com>
2022-10-17 12:38:15 -0500

    Initial commit

---
+++ app/src/main/java/org/idnp/labidnp_03/MenuActivity.java
@@ -0,0 +1,14 @@
+package org.idnp.labidnp_03;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
